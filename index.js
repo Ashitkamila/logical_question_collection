fea83626d84cc815513cccb87b76a9ded093db97
Ashitkamila <86040526+Ashitkamila@users.noreply.github.com>
2022-08-19 12:24:25 +0530

    Update index.js

--- index.js
+++ index.js
@@ -367,3 +367,25 @@ function maxSum(arr){
   return a2
 }
 console.log(maxSum(array));
+
+//find permutation
+let findPermutations = (string) => {
+  if (!string || typeof string !== "string"){
+    return "Please enter a string"
+  } else if (string.length < 2 ){
+    return string
+  }
+
+  let permutationsArray = [] 
+   
+  for (let i = 0; i < string.length; i++){
+    let char = string[i];
+
+    let remainingChars = string.slice(0, i) + string.slice(i + 1, string.length);
+  
+    for (let permutation of findPermutations(remainingChars)){
+      permutationsArray.push(char + permutation) }
+  }
+  return permutationsArray
+}
+console.log(findPermutations('abc'));
