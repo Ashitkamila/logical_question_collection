dc993504eebd7984569676d8cb1fdf5362d1ccc7
Ashitkamila <86040526+Ashitkamila@users.noreply.github.com>
2023-08-28 20:22:50 +0530

    Update index.js

--- index.js
+++ index.js
@@ -765,3 +765,23 @@ let filterOfAllDuplicate = findAllBooks.filter((val,ind,array)=>{
 let secondMethod = [... new Set(findAllBooks)];
 // console.log(filterOfAllDuplicate)
 console.log(secondMethod)
+//You are climbing a staircase. It takes n steps to reach the top. Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
+
+const climbStairs = (n) => {
+  if (n === 0 || n === 1) {
+    return 1;
+  }
+
+  const dp = new Array(n + 1);
+  dp[0] = 1;
+  dp[1] = 1;
+
+  for (let i = 2; i <= n; i++) {
+    dp[i] = dp[i - 1] + dp[i - 2];
+  }
+
+  return dp[n];
+};
+
+const steps = 10; // Example: 5 steps
+console.log(`Number of ways to climb ${steps} stairs: ${climbStairs(steps)}`);
